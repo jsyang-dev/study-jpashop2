2fca0a3480f954487aebf676191ebfc9b69cf591
Jeongsu Yang <mycat83@gmail.com>
2022-02-28 00:12:46 +0900

    feat: 간단한 주문 조회 V1: 엔티티를 직접 노출

--- src/main/java/me/study/jpashop2/api/OrderSimpleApiController.java
+++ src/main/java/me/study/jpashop2/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package me.study.jpashop2.api;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import me.study.jpashop2.domain.Address;
 import me.study.jpashop2.domain.Order;
+import me.study.jpashop2.domain.OrderStatus;
 import me.study.jpashop2.repository.OrderRepository;
 import me.study.jpashop2.repository.OrderSearch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * ManyToOne, OneToOne
@@ -26,5 +31,32 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();   // Lazy 강제 초기화
         }
         return orders;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        // 주문 2개
+        // N + 1 -> 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    private static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
